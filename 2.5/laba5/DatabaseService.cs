67ea2c9a6bbe6440a9d5eb7f571312060ad56c59
workinfox <feel.fox34@gmail.com>
2024-04-23 10:12:14 +0700

    first commit

---
+++ 2.5/laba5/DatabaseService.cs
@@ -0,0 +1,23 @@
+﻿using Npgsql;
+using System;
+
+namespace laba5;
+public static class DatabaseService
+{
+    private static NpgsqlConnection? _connection;
+    private static string GetConnectionString()
+    {
+        return @"Host=10.30.0.137;Database=gr622_krami;Username=gr622_krami;Password=02112006Arinak";
+    }
+
+    public static NpgsqlConnection GetSqlConnection()
+    {
+        if (_connection is null)
+        {
+            _connection = new NpgsqlConnection(GetConnectionString());
+            _connection.Open();
+        }
+        
+        return _connection;
+    }
+}
