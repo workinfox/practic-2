db2ba4bbe7d1b31c0cc2d98907873b88d59604e5
workinfox <feel.fox34@gmail.com>
2024-04-25 13:09:55 +0700

    first commit

---
+++ 2.5/laba25/laba25/DatabaseRequests.cs
@@ -0,0 +1,131 @@
+﻿
+using Npgsql;
+using System;
+
+namespace laba25;
+public static class DatabaseRequests
+{
+    public static void GetTypeCarQuery()
+    {
+        var querySql = "SELECT * FROM type_car";
+        using var cmd = new NpgsqlCommand(querySql, DatabaseService.GetSqlConnection());
+        using var reader = cmd.ExecuteReader();
+        
+        while (reader.Read())
+        {
+            Console.WriteLine($"Id: {reader[0]} Название: {reader[1]}");
+        }
+    }
+
+    public static void AddTypeCarQuery(string name)
+    {
+        var querySql = $"INSERT INTO type_car(name) VALUES ('{name}')";
+        using var cmd = new NpgsqlCommand(querySql, DatabaseService.GetSqlConnection());
+        cmd.ExecuteNonQuery();
+    }
+
+    public static void AddDriverQuery(string firstName, string lastName, DateTime birthdate)
+    {
+        var querySql = $"INSERT INTO driver(first_name, last_name, birthdate) VALUES ('{firstName}', '{lastName}', '{birthdate}')";
+        using var cmd = new NpgsqlCommand(querySql, DatabaseService.GetSqlConnection());
+        cmd.ExecuteNonQuery();
+    }
+
+    public static void GetDriverQuery()
+    {
+        var querySql = "SELECT * FROM driver";
+        using var cmd = new NpgsqlCommand(querySql, DatabaseService.GetSqlConnection());
+        using var reader = cmd.ExecuteReader();
+
+        while (reader.Read())
+        {
+            Console.WriteLine($"Id: {reader[0]} Имя: {reader[1]} Фамилия: {reader[2]} Дата рождения: {reader[3]}");
+        }
+    }
+
+    public static void AddRightsCategoryQuery(string name)
+    {
+        var querySql = $"INSERT INTO rights_category(name) VALUES ('{name}')";
+        using var cmd = new NpgsqlCommand(querySql, DatabaseService.GetSqlConnection());
+        cmd.ExecuteNonQuery();
+    }
+
+    public static void AddDriverRightsCategoryQuery(int driver, int rightsCategory)
+    {
+        var querySql = $"INSERT INTO driver_rights_category(id_driver, id_rights_category) VALUES ({driver}, {rightsCategory})";
+        using var cmd = new NpgsqlCommand(querySql, DatabaseService.GetSqlConnection());
+        cmd.ExecuteNonQuery();
+    }
+
+    public static void GetDriverRightsCategoryQuery(int driver)
+    {
+        var querySql = "SELECT dr.first_name, dr.last_name, rc.name " +
+                       "FROM driver_rights_category " +
+                       "INNER JOIN driver dr on driver_rights_category.id_driver = dr.id " +
+                       "INNER JOIN rights_category rc on rc.id = driver_rights_category.id_rights_category " +
+                       $"WHERE dr.id = {driver};";
+        using var cmd = new NpgsqlCommand(querySql, DatabaseService.GetSqlConnection());
+        using var reader = cmd.ExecuteReader();
+
+        while (reader.Read())
+        {
+            Console.WriteLine($"Имя: {reader[0]} Фамилия: {reader[1]} Категория прав: {reader[2]}");
+        }
+    }
+    public static void GetRightsCategoryQuery()
+    {
+        var querySql = "SELECT * FROM rights_category";
+        using var cmd = new NpgsqlCommand(querySql, DatabaseService.GetSqlConnection());
+        using var reader = cmd.ExecuteReader();
+
+        while (reader.Read())
+        {
+            Console.WriteLine($"Id: {reader[0]} Категория прав: {reader[1]}");
+        }
+    }
+    
+    public static void GetItineraryQuery()
+    {
+        var querySql = "SELECT * FROM itinerary";
+        using var cmd = new NpgsqlCommand(querySql, DatabaseService.GetSqlConnection());
+        using var reader = cmd.ExecuteReader();
+
+        while (reader.Read())
+        {
+            Console.WriteLine($"Id: {reader[0]} Маршрут: {reader[1]}");
+        }
+    }
+    
+    public static void GetRouteQuery()
+    {
+        var querySql = "SELECT * FROM route";
+        using var cmd = new NpgsqlCommand(querySql, DatabaseService.GetSqlConnection());
+        using var reader = cmd.ExecuteReader();
+
+        while (reader.Read())
+        {
+            Console.WriteLine($"Id: {reader[0]} Id водителя: {reader[1]} Id автомобиля: {reader[2]} Id маршрута: {reader[3]} Число пассажиров: {reader[4]}");
+        }
+    }
+    
+    public static void AddCarsQuery(int idTypeCar, string name, string number, int capacity)
+    {
+        var querySql = $"INSERT INTO car(id_type_car, name, state_number, number_passengers) VALUES ({idTypeCar}, '{name}', '{number}', {capacity})";
+        using var cmd = new NpgsqlCommand(querySql, DatabaseService.GetSqlConnection());
+        cmd.ExecuteNonQuery();
+    }
+    
+    public static void AddItineraryQuery(string name)
+    {
+        var querySql = $"INSERT INTO itinerary(name) VALUES ('{name}')";
+        using var cmd = new NpgsqlCommand(querySql, DatabaseService.GetSqlConnection());
+        cmd.ExecuteNonQuery();
+    }
+    
+    public static void AddRouteQuery(int idDriver, int idCar, int idItinerary, int numberPassengers)
+    {
+        var querySql = $"INSERT INTO route(id_driver, id_car, id_itinerary, number_passengers) VALUES ({idDriver}, {idCar}, {idItinerary}, {numberPassengers})";
+        using var cmd = new NpgsqlCommand(querySql, DatabaseService.GetSqlConnection());
+        cmd.ExecuteNonQuery();
+    }
+}
